e353d8c5b0be9ce65932596d5df18374fb0d3521
Taush Sampley <taushsampley@gmail.com>
2024-01-28 01:37:29 -0600

    Create application window and fill with collision view

--- app-desktop/src/main/kotlin/com/taushsampley/collisions/Main.kt
+++ app-desktop/src/main/kotlin/com/taushsampley/collisions/Main.kt
@@ -1,12 +1,27 @@
 package com.taushsampley.collisions
 
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
-import com.taushsampley.collisions.ui.CollisionGrapher
+import androidx.compose.ui.window.rememberWindowState
+import com.taushsampley.collisions.ui.CollisionView
+import com.taushsampley.collisions.ui.rememberCollisionState
 
 /**
  *
  */
-fun main(vararg args: String) = application {
+fun main() = application {
 
-    CollisionGrapher()
+    Window(
+        onCloseRequest = this::exitApplication,
+        title = "",
+        state = rememberWindowState()
+    ) {
+        CollisionView(
+            state = rememberCollisionState(),
+            modifier = Modifier.fillMaxSize()
+        )
+    }
 }
