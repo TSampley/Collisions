ba68e90848fa90b1077f81be6b6aebb8a7374859
Taush Sampley <taushsampley@gmail.com>
2024-01-28 01:52:50 -0600

    Replace generated GreetingView with CollisionView

--- app-android/src/main/java/com/taushsampley/collisions/android/MainActivity.kt
+++ app-android/src/main/java/com/taushsampley/collisions/android/MainActivity.kt
@@ -5,10 +5,9 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.*
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.taushsampley.collisions.Greeting
+import com.taushsampley.collisions.ui.CollisionView
+import com.taushsampley.collisions.ui.rememberCollisionState
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,22 +18,12 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    GreetingView(Greeting().greet())
+                    CollisionView(
+                        state = rememberCollisionState(),
+                        modifier = Modifier.fillMaxSize()
+                    )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun GreetingView(text: String) {
-    Text(text = text)
-}
-
-@Preview
-@Composable
-fun DefaultPreview() {
-    MyApplicationTheme {
-        GreetingView("Hello, Android!")
     }
 }
